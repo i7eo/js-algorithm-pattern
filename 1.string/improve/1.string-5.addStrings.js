11485be3c626ecc89d5ee7bc71c8dda76ba28b32
George.Tan <406132614@qq.com>
2020-09-18 11:48:33 +0800

    update

---
+++ 1.string/improve/1.string-5.addStrings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    var i = num1.length - 1,
+        j = num2.length - 1,
+        fill = 0,
+        ans = [];
+
+    // '1' + '9' 此时 fill 为 0 所以加上 fill != 0 需要再来一次
+    while(i >= 0 || j >= 0 || fill != 0) {
+        // 将字符串转为数字，高位不够就补0
+        var ic = i>=0 ? num1.charAt(i) - 0: 0;
+        var jc = j>=0 ? num2.charAt(j) - 0: 0;
+        var result = ic + jc + fill;
+        fill = Math.floor(result/10);
+        ans.unshift(result % 10)
+        i --;
+        j --;
+    }
+    return ans.join('')
+};
